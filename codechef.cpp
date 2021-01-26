c9a09e3b20bcafe8f234e0428c17b8ff8a3c0fee
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2021-01-26 19:57:56 +0530

    Update codechef.cpp

--- codechef.cpp
+++ codechef.cpp
@@ -1,69 +1,96 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector< vector<int> > final_ans;
-vector<int> arr;
-int min_lenght = INT_MAX;
-void find_subone(vector<int> prev, int index, int sum, int k)
+struct Node
 {
-	if (index == arr.size())
+	int data;
+	Node *left;
+	Node *right;
+};
+Node *root = NULL;
+unordered_map<int, Node *> map_;
+void traversal(Node *root,string s)
+{
+	if(!root->left && !root->right)
 	{
-		if (sum >= k && prev.size() <= min_lenght)
-		{
-			final_ans.push_back(prev);
-			min_lenght = prev.size();
-		}
-		return;
+		// cout<<s<<" ";
+		return ;
 	}
-	find_subone(prev, index + 1, sum, k);
-	prev.push_back(arr[index]);
-	find_subone(prev, index + 1, sum + arr[index], k);
+	cout<<root->data<<" ";
+	traversal(root->left,s+='0');
+	s.pop_back();
+	traversal(root->right,s+='1');
+	s.pop_back();
 }
-
 int main()
 {
-	int t;
-	cin >> t;
-	while (t--)
+	int n;
+	cin >> n;
+	priority_queue<int> q;
+	for (int i = 0; i < n; i++)
 	{
-		int ans=0;
-		vector<int> seive(10000);
-		arr.clear();
-		final_ans.clear();
-		min_lenght = INT_MAX;
-		int n, k;
-		cin >> n >> k;
-		for (int i = 0; i < n; i++)
+		int value;
+		cin >> value;
+		q.push((value) * (-1));
+	}
+	while (q.size()!=1)
+	{
+		// cout<<q.size();
+		int val1 = q.top() * -1;
+		q.pop();
+		int val2 = q.top() * -1;
+		q.pop();
+		// cout<<val1<<" "<<val2<<endl;
+		Node *temp;
+		temp = new Node;
+		temp->data = val1 + val2;
+		if (map_.find(val1) == map_.end() && map_.find(val2) == map_.end())
 		{
-			int var;
-			cin >> var;
-			arr.push_back(var);
+			Node *node1 = new Node;
+			node1->data = val1;
+			node1->left = node1->right = NULL;
+			Node *node2 = new Node;
+			node2->data = val2;
+			node2->left = node2->right = NULL;
+			temp->left = node1;
+			temp->right = node2;
+			map_.insert(make_pair(temp->data, temp));
 		}
-		vector<int> prev;
-		find_subone(prev, 0, 0, k);
-		if (final_ans.size() == 0)
-			cout << "-1" << endl;
+		else if (map_.find(val1) != map_.end() && map_.find(val2) != map_.end())
+		{
+			// cout<<val1<<" "<<val2<<endl;
+			temp->left = map_[val1];
+			temp->right = map_[val2];
+			map_.erase(val2);
+			map_.erase(val1);
+			map_.insert(make_pair(temp->data, temp));
+		}
+		else if (map_.find(val2) != map_.end() && map_.find(val1) == map_.end())
+		{
+			Node *node1 = new Node;
+			node1->data = val1;
+			node1->left = node1->right = NULL;
+			temp->left = node1;
+			temp->right = map_[val2];
+			map_.erase(val2);
+			map_.insert(make_pair(temp->data, temp));
+		}
 		else
 		{
-			for (int i = 0; i < final_ans.size(); i++)
-			{
-				if (final_ans[i].size() != min_lenght)
-					final_ans.erase(final_ans.begin() + i);
-			}
-			if (final_ans.size() > 1)
-			{
-				
-			}
+			Node *node1 = new Node;
+			node1->data = val2;
+			node1->left = node1->right = NULL;
+			temp->left = map_[val1];
+			temp->right = node1;
+			map_.erase(val1);
+			map_.insert(make_pair(temp->data, temp));
 		}
+		if(q.empty())
+			root=temp;
+		// traversal(temp);
+		// cout<<endl;
+		q.push((val1 + val2) * -1);
 	}
+	traversal(root,"");
 	return 0;
 }
-
-
-
-// for (int i = 0; i < final_ans.size(); i++)
-// 		{
-// 			for (int j = 0; j < final_ans[i].size(); j++)
-// 				cout << final_ans[i][j] << " ";
-// 			cout << endl;
-// 		}
